0bb6ed380ff8e5516608e941d1758b2ea53e6fe1
jhoneagle <eaglekjohn@gmail.com>
2018-11-17 14:38:12 +0200

    localhost works only atm

--- config/environment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="oolli"
-# korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="miniohtu"
-# sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
