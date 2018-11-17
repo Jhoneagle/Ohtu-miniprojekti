d9340d5f736d3a7981674956df90c0591ec7f170
PikkuHukka <olli.x.ojala@helsinki.fi>
2018-11-17 13:27:08 +0200

    Shadow, gradle ja user 2017

---
+++ config/environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# korvaa tämä omalla käyttäjätunnuksellasi
+USERNAME="oolli"
+# korvaa tämä haluamallasi kansion nimellä
+PROJECT_FOLDER="miniohtu"
+# sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
