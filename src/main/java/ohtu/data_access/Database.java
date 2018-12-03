bde517d8f31af38a77630b8ba3f2796198acfdf3
jhoneagle <eaglekjohn@gmail.com>
2018-12-03 13:31:21 +0200

    postgres connection fix

--- src/main/java/ohtu/data_access/Database.java
+++ src/main/java/ohtu/data_access/Database.java
@@ -1,26 +1,42 @@
 
 package ohtu.data_access;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Database {
     private final String databaseAddress;
+    private final boolean test;
+    private final String env;
     
     public Database(String databaseAddress, boolean test) {
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        
-        if (dbUrl != null && !test) {
-            this.databaseAddress = dbUrl;
-        } else {
-            this.databaseAddress = databaseAddress;
-        }
+        this.databaseAddress = databaseAddress;
+        this.test = test;
+        this.env = System.getenv("DATABASE_URL");
         
         initializeSqlTables();
     }
     
     public Connection getConnection() throws SQLException {
+        if (this.env != null && !test) {
+            try {
+                URI dbUri = new URI(this.env);
+                
+                String username = dbUri.getUserInfo().split(":")[0];
+                String password = dbUri.getUserInfo().split(":")[1];
+                String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+                
+                return DriverManager.getConnection(dbUrl, username, password);
+            } catch (URISyntaxException ex) {
+                System.out.println("URL fail!");
+            }
+        }
+        
         return DriverManager.getConnection(databaseAddress);
     }
     
