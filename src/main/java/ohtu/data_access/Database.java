52b2e6b97fa2b627e22adf77151b41ceecc739ba
jhoneagle <eaglekjohn@gmail.com>
2018-12-09 22:16:29 +0200

    extend sql varchar size

--- src/main/java/ohtu/data_access/Database.java
+++ src/main/java/ohtu/data_access/Database.java
@@ -29,10 +29,10 @@ public class Database {
         if (this.test || System.getenv("JDBC_DATABASE_URL") == null) {
             try {
                 getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS Vinkki(id INTEGER PRIMARY KEY, "
-                        + "otsikko varchar(255), kuvaus varchar(255), tekija varchar(100), linkki varchar(255)"
+                        + "otsikko varchar(255), kuvaus varchar(10000), tekija varchar(100), linkki varchar(255)"
                         + ", tagit varchar(255), luettu date, isbn varchar(255))").execute();
                 getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS Kommentti(id INTEGER PRIMARY KEY, "
-                        + "vinkki_id INTEGER, nikki varchar(255), content varchar(255), created date, "
+                        + "vinkki_id INTEGER, nikki varchar(255), content varchar(10000), created date, "
                         + "FOREIGN KEY (vinkki_id) REFERENCES Vinkki(id))").execute();
             } catch (SQLException ex) {
                 System.out.println("create tables failed!" + "\n" + ex);
@@ -40,10 +40,10 @@ public class Database {
         } else {
             try {
                 getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS Vinkki(id SERIAL PRIMARY KEY NOT NULL, "
-                        + "otsikko varchar(255), kuvaus varchar(255), tekija varchar(100), linkki varchar(255)"
+                        + "otsikko varchar(255), kuvaus varchar(10000), tekija varchar(100), linkki varchar(255)"
                         + ", tagit varchar(255), luettu date, isbn varchar(255))").execute();
                 getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS Kommentti(id SERIAL PRIMARY KEY NOT NULL, "
-                        + "vinkki_id INTEGER, nikki varchar(255), content varchar(255), created date, "
+                        + "vinkki_id INTEGER, nikki varchar(255), content varchar(10000), created date, "
                         + "FOREIGN KEY (vinkki_id) REFERENCES Vinkki(id))").execute();
             } catch (SQLException ex) {
                 System.out.println("create tables failed!" + "\n" + ex);
